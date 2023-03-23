42b56a42d2776ac32245bf231752af2322aa9680
JMOakland <jmencotti@oakland.edu>
2023-03-23 14:54:26 -0400

    fix try 3

--- Final_Project/javascript/index.js
+++ Final_Project/javascript/index.js
@@ -4,9 +4,7 @@ const images = slides.querySelectorAll('img');
 const slideshow2 = document.querySelector('.slideshow2');
 const slides2 = slideshow2.querySelector('.slides2');
 const images2 = slides2.querySelectorAll('img');
-const slideshow3 = document.querySelector('.slideshow3');
-const slides3 = slideshow3.querySelector('.slides3');
-const images3 = slides3.querySelectorAll('img');
+
 function play(){
     let count = 0;
     setInterval(function loop() {
@@ -33,19 +31,7 @@ function play2(){
 
 
 }
-function play3(){
-    let count = 0;
-    setInterval(function loop() {
-    count += 1;
 
-    if(count === images3.length){
-     count = 0;
-     }
-        slides3.style.transform = `translate3d(${count * -705}px, 0, 0)`;
-    } ,3000) 
-
-
-}
 play();
 play2();
-play3();
+
