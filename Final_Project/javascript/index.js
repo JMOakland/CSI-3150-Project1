30298a73a4d16c2a37993463f8aadb3fa753101b
JMOakland <jmencotti@oakland.edu>
2023-03-23 14:26:03 -0400

    JS usage update
    made it so that the JS is now only in index.js. this is a test to see if it works from github pages.

--- Final_Project/javascript/index.js
+++ Final_Project/javascript/index.js
@@ -1,19 +1,35 @@
 const slideshow = document.querySelector('.slideshow');
-const slides = slideshow.querySelector('.slides');
- const images = slides.querySelectorAll('img');
-
+    const slides = slideshow.querySelector('.slides');
+    const images = slides.querySelectorAll('img');
+    const slideshow2 = document.querySelector('.slideshow2');
+    const slides2 = slideshow2.querySelector('.slides2');
+    const images2 = slides2.querySelectorAll('img');
 function play(){
     let count = 0;
     setInterval(function loop() {
     count += 1;
 
     if(count === images.length){
-        count = 0;
-    }
-    slides.style.transform = `translate3d(${count * 1720}px, 0, 0)`;
-   } ,3000) 
+     count = 0;
+     }
+        slides.style.transform = `translate3d(${count * -705}px, 0, 0)`;
+    } ,3000) 
 
-    
+
+}
+function play2(){
+    let count = 0;
+    setInterval(function loop() {
+    count += 1;
+
+    if(count === images2.length){
+     count = 0;
+     }
+        slides2.style.transform = `translate3d(${count * -705}px, 0, 0)`;
+    } ,3000) 
+
+
 }
 
 play();
+play2();
